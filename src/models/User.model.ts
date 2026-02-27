b5104e81174983c12279b163e1b6fcf80b9e2690
SayanBiswas23 <bsayan238@gmail.com>
2026-02-27 14:43:09 +0530

    Add mongoose dependency and create user/message schemas
    - Added mongoose as a dependency in package.json
    - Created user and message schemas in User.model.ts
    - Added new schema files: acceptMessageSchema.ts, messageSchema.ts, signinSchema.ts, signupSchema.ts, and verifySchema.ts

---
+++ src/models/User.model.ts
@@ -0,0 +1,70 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+	content: String;
+	createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+	content: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		required: true,
+		default: Date.now,
+	},
+});
+
+export interface User extends Document {
+	username: String;
+	email: String;
+	password: String;
+	createdAt: Date;
+	verifyCode: String;
+	verifyCodeExpiry: String;
+	isVerified: Boolean;
+	isAccecptingMessages: Boolean;
+	messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: [true, "Email is required"],
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, "Password is required"],
+	},
+	verifyCode: {
+		type: String,
+		required: [true, "Verify code is required"],
+	},
+	verifyCodeExpiry: {
+		type: String,
+		required: [true, "Verify code expiry is required"],
+	},
+	isVerified: {
+		type: Boolean,
+		default: false,
+	},
+	isAccecptingMessages: {
+		type: Boolean,
+		required: true,
+	},
+	messages: [MessageSchema],
+});
+
+
+const UserModel =( mongoose.models.User  as mongoose.Model<User>) ||mongoose.model<User>("User",UserSchema)
+
+
+export default UserModel;
